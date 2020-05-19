a9739569158618eb4ae1dcdf52052d7738e74c1e
harrychor <58283630+harrychor@users.noreply.github.com>
2020-05-19 18:52:03 +0800

    login test

--- ng7-pre/src/app/login/login.component.ts
+++ ng7-pre/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() { 
+  }
+  onRegister(){
+  }
+  
+  login(event) {
+    event.preventDefault()
+    console.log(event)
   }
 
+  
 }
+
+
