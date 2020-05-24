a5181689a977fc561c62d02f2af4c9218a324aeb
harrychor <58283630+harrychor@users.noreply.github.com>
2020-05-24 16:09:13 +0800

    fav delete update

--- ng7-pre/src/app/contact/contact.component.ts
+++ ng7-pre/src/app/contact/contact.component.ts
@@ -8,21 +8,30 @@ import{ConfirmationPopoverModule} from 'angular-confirmation-popover';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  showModal: boolean;
 
   constructor(private data: DataService) { }
   i:Number;
   moives:Object;
+  hide(){this.showModal = false;}
+  open(i:number){this.showModal = true;
+  sessionStorage.setItem('deleteID',i.toString());
+  }
+
   ngOnInit(): void {
+    
     this.data.getFavData().subscribe(data=>{
       this.moives = data;	
       console.log(this.moives)
       
       })
   }
-  delFav(i:number){
+  delFav(){
+    const i = sessionStorage.getItem('deleteID');
     console.log(this.moives[i]['_id'])
     const delID = this.moives[i]['_id']
+    this.hide()
     //window.alert();
-    //this.data.delFavData(delID)
+    this.data.delFavData(delID)
   }
 }
